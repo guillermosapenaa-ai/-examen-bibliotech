0d9f4243731aa5b5f573efb6826dc8ea042d7eb1
Guillermo <guillermosapenaa@gmail.com>
2026-05-10 16:53:18 +0200

    Ejercicio 1

---
+++ Examen_Simulacro/Libro.cs
@@ -0,0 +1,27 @@
+﻿public class Libro
+{
+
+	private string titulo;
+	private string autor;
+	private int anyo;
+	private bool disponible;
+
+
+	public Libro(string titulo, string autor, int anyo, bool disponible)
+	{
+		this.titulo = titulo;
+		this.autor = autor;
+		this.anyo = anyo;
+		this.disponible = disponible;
+	}
+
+	public string getTitulo() { return titulo; }
+	public string getAutor() { return autor; }
+	public int getAnyo() { return anyo; }
+	public bool isDisponible() { return disponible; }
+
+	public override string ToString()
+	{
+		return $"{titulo} - {autor} ({anyo})";
+	}
+}
